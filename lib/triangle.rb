5af62448cd949ccde8241c912629550762093228
github username <github email address>
2019-07-01 09:17:17 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
-end
+  
+  attr_accessor :adjacent, :opposite, :hypotenuse
+
+  def initialize(adjacent, opposite, hypotenuse)
+    @adjacent = adjacent
+    @opposite = opposite
+    @hypotenuse = hypotenuse
+  end 
+
+  def kind 
+    if adjacent <= 0 || opposite <= 0 || hypotenuse <= 0 
+      raise TriangleError
+    elsif adjacent + opposite <= hypotenuse || opposite + hypotenuse <= adjacent || adjacent + hypotenuse <= opposite 
+      raise TriangleError 
+    elsif adjacent == opposite && opposite == hypotenuse 
+      :equilateral 
+    elsif adjacent == opposite || opposite == hypotenuse || adjacent == hypotenuse 
+      :isosceles 
+    else 
+      :scalene
+    end 
+  end 
+
+  class TriangleError < StandardError
+  end
+end 
